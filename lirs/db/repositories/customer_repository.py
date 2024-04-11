48892a8d0721c2afe9336ae94d469cadc3558bad
Augusto M Splett <augusto.splett@gmail.com>
2024-04-10 21:20:48 -0400

    Created the .gitignore and deleted the caches

--- lirs/db/repositories/customer_repository.py
+++ lirs/db/repositories/customer_repository.py
@@ -26,15 +26,43 @@ class CustomerRepository:
         except mysql.connector.Error as e:
             print(f"Erro to insert the customer: {e}")
 
+    def get_customers(self) -> list[Customer]:
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM inn_customer"
+            cursor.execute(sql)
+            records = cursor.fetchall()
+            return records
 
-    def get_customer_by_id(self, id:int):
-        # Implementação para ler um registro
-        pass
+    def get_customer_by_id(self, id:int)-> Customer:
+            sql = "SELECT * FROM inn_customer where id = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, id)
+            record = cursor.fetchone()
+            if record:
+                return Customer(id=record[0], first_name=record[1], last_name=record[2], email=record[3], phone_number=record[4])
+            else:
+                return None
 
     def update_customer(self, id: int, customer: Customer):
-        # Implementação para atualizar um registro
-        pass
+        cursor = connection.cursor()
+        sql = "UPDATE tabela_exemplo SET nome = %s, idade = %s WHERE id = %s"
+        values = (nome, idade, id)
+        cursor.execute(sql, values)
+        connection.commit()
+        print("Registro atualizado com sucesso!")
+
+    # Exemplo de uso:
+    # connection = mysql.connector.connect(...)  # Conectar ao banco de dados
+    # update_record(connection, 1, "Maria Silva", 35)
 
     def delete_customer(self, id: int):
-        # Implementação para excluir um registro
-        pass
+        cursor = connection.cursor()
+        sql = "DELETE FROM tabela_exemplo WHERE id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        connection.commit()
+        print("Registro excluído com sucesso!")
+
+    # Exemplo de uso:
+    # connection = mysql.connector.connect(...)  # Conectar ao banco de dados
+    # delete_record(connection, 1)
