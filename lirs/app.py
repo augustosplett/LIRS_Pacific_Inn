0f6efe12aa6291be99c48bb43ba9830d53847cef
Augusto M Splett <augusto.splett@gmail.com>
2024-04-12 16:21:33 -0400

    Created the functions to validate if the database exists and create it if necesssary

--- lirs/app.py
+++ lirs/app.py
@@ -5,19 +5,19 @@ import os
 from db.models.customer_model import Customer
 from db.repositories.customer_repository import CustomerRepository
 
-# #get the current path
-# path = os.getcwd()
-# #build the full db config path
-# full_configuration_path = path + '/lirs/config/database.ini'
+#get the current path
+path = os.getcwd()
+#build the full db config path
+full_configuration_path = path + '/lirs/config/database.ini'
 
-# #connect to the db considering the information on the configuration file 
-# conexao = connect_to_mysql(full_configuration_path)
+#connect to the db considering the information on the configuration file 
+#conexao = connect_to_mysql(full_configuration_path)
 
 #call the main menu
 #menu()
-
-test = CustomerRepository()
-customer = Customer( "Priscilla", "Luz", "augusto@gmail.com", 4381112222)
-print(customer)
+check_db_exist(full_configuration_path)
+# test = CustomerRepository()
+# customer = Customer( "Priscilla", "Luz", "augusto@gmail.com", 4381112222)
+# print(customer)
 #test.update_customer(2, customer)
 #test.delete_customer(2)
--- lirs/db/db_utils.py
+++ lirs/db/db_utils.py
@@ -30,5 +30,98 @@ def connect_to_mysql(configuration_file: str):
             #print("Conectado ao banco de dados MySQL")
             return connection
     except mysql.connector.Error as e:
-        print(f"Erro ao conectar ao banco de dados MySQL: {e}")
+        print(f"Error to connect MySQL: {e}")
     return None
+
+def db_exists(cursor, db_name: str)->bool:
+    '''
+        Rerturn if the desired database exists or not
+    '''
+    try:
+        cursor.execute("SHOW DATABASES")
+        dbs = [db[0] for db in cursor.fetchall()]
+        
+        if db_name not in dbs:
+            return False
+        else:
+            return True
+    except mysql.connector.Error as e:
+        print(f"Error to validade if the database exists: {e}")
+
+
+def create_db(cursor, db_name):
+    '''
+    Create the tables using the given cursor on the given db
+    '''
+    try:
+        #if database doesn't exist, create it
+        cursor.execute("CREATE DATABASE {}".format(db_name))
+        #use the created database
+        cursor.execute("USE {}".format(db_name))
+        #Create the tables
+        scripts_sql = [
+            """
+            CREATE TABLE `inn_customer` (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `first_name` varchar(50) DEFAULT NULL,
+                `last_name` varchar(50) DEFAULT NULL,
+                `email` varchar(50) DEFAULT NULL,
+                `phone_number` bigint DEFAULT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """,
+            """
+            CREATE TABLE `inn_rooms` (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `room_type` varchar(1) DEFAULT NULL,
+                `room_price` decimal(5,2) DEFAULT NULL,
+                `avaliability` smallint DEFAULT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """,
+            """
+            CREATE TABLE `inn_reservation` (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `room_id` int DEFAULT NULL,
+                `customer_id` int DEFAULT NULL,
+                `accomodation_days` smallint DEFAULT NULL,
+                `cost` decimal(5,2) DEFAULT NULL,
+                `checkout` tinyint DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                KEY `room_id` (`room_id`),
+                KEY `customer_id` (`customer_id`),
+                CONSTRAINT `inn_reservation_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `inn_rooms` (`id`),
+                CONSTRAINT `inn_reservation_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `inn_customer` (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """ 
+        ]
+        # execute the scripts to create the tables
+        for script in scripts_sql:
+            cursor.execute(script)
+    except mysql.connector.Error as e:
+        print(f"Error to create tables: {e}")
+
+def check_db_exist(configuration_file):
+    '''
+    Function responsible for validate if the database exists
+    '''
+    #read db configurations
+    config = read_db_configuration(configuration_file)
+    try:
+        #stablish the connection
+        conn = mysql.connector.connect(
+                host=config['host'],
+                user=config['user'],
+                password=config['password']
+            )
+
+        # Cursor para executar comandos SQL
+        with conn.cursor() as cursor:
+            exist = db_exists(cursor,config['database'] )
+
+            if not exist:
+                create_db(cursor,config['database'])
+            
+        conn.close()
+    except mysql.connector.Error as e:
+        print(f"Error to validade if the database exists: {e}")
