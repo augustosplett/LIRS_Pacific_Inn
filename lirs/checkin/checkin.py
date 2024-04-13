49cbad521d0e8bae2619e85037eb8fe2f85aed61
Augusto M Splett <augusto.splett@gmail.com>
2024-04-13 15:34:44 -0400

    created function to start the checkin, capture the room, and the client. If client doens't exists, create a new one.

--- lirs/checkin/checkin.py
+++ lirs/checkin/checkin.py
@@ -1,5 +1,8 @@
+import re
 from db.repositories.rooms_repository import RoomsRepository
+from db.repositories.customer_repository import CustomerRepository
 from db.models.rooms_model import Room
+from db.models.customer_model import Customer
 
 class Checkin:
 
@@ -10,11 +13,39 @@ class Checkin:
         print("________________________________________________________________")
         selected_room = self.handle_room_selection()
         if selected_room is not None:
-            self.handle_customer_information()
+            customer = self.handle_customer_information()
 
-    def handle_customer_information():
-        pass
-    
+    def validate_email(self, email):
+        '''
+        Validate if the input email is valid
+        '''
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return bool(re.match(pattern, email))
+
+    def handle_customer_information(self)-> Customer:
+        #asks for email to try to find if the user already exists:
+        customer_email = input("Please, enter customer's email:")
+        while not self.validate_email(customer_email):
+            print("Please, enter a valid email e.g.: 'name@copr.com'")
+            customer_email = input("Please, enter customer's email:")
+        #validate if the user already exists:
+        c_repository = CustomerRepository()
+        customer = c_repository.get_customer_by_email(customer_email)
+        #create the customer if it doens't exist
+        if customer is None:
+            print("Customer not found, please, fill the customer information to create one:")
+            #handle the customer creation
+            first_name = input("First Name:")
+            last_name = input("Last Name:")
+            phone = int(input("Phone Number:"))
+            c_repository.create_customer(Customer(first_name=first_name, 
+                                                  last_name=last_name,
+                                                  email=customer_email,
+                                                  phone_number=phone))
+            customer = c_repository.get_customer_by_email(customer_email)
+        #return customer
+        return customer
+
     def handle_room_selection(self) -> Room:
         ''' 
         Function responsible for printitng the room information in the screen and capture the user selection
